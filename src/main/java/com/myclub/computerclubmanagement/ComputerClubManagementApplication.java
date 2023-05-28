33bddc69e2e2cdb8f4c16b7a2a18adc0f7cb96c1
usevalad <Seva552.seva@gmail.com>
2023-05-28 21:18:37 +0200

    init commit

---
+++ src/main/java/com/myclub/computerclubmanagement/ComputerClubManagementApplication.java
@@ -0,0 +1,13 @@
+package com.myclub.computerclubmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerClubManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComputerClubManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myclub/computerclubmanagement/ComputerClubManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myclub.computerclubmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerClubManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
